15b64594938d561fd5dccb24ad51557ab0f21668
Dev Oyw <siriusoyw@outlook.com>
2023-10-10 21:58:04 +0800

    添加项目文件。

---
+++ TestNTreeNode/Models/BaseNTreeNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNTreeNode.Models
+{
+    public class BaseNTreeNode<TKey> : BaseEntity<TKey>
+    {
+        public string CreatedAt { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string ModifiedAt { get; set; }
+        public DateTime ModifiedTime { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ TestNTreeNode/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNTreeNode.Models
+{
+    public class User : BaseNTreeNode<Guid>
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Sex { get; set; }
+        public Role Role { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin = 1,
+        User = 2
+    }
+}
